07b4d2a5d0596a33391754ccd1282ef094bb0af0
lbt05 <lbt0506@gmail.com>
2016-07-11 11:36:53 +0800

    update root endpont

--- src/main/java/cn/leancloud/demo/todo/App.java
+++ src/main/java/cn/leancloud/demo/todo/App.java
@@ -1,13 +1,23 @@
 package cn.leancloud.demo.todo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@RestController
+@EnableAutoConfiguration
+public class App {
+  @RequestMapping("/")
+  String home() {
+    return "Hello World!";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
 }
